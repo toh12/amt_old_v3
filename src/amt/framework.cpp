9e890d1e5d1428af9d8026072a95c1c8f51e0435
toh12 <61188664+toh12@users.noreply.github.com>
2022-01-21 09:26:38 +0000

    Update framework.cpp

--- src/amt/framework.cpp
+++ src/amt/framework.cpp
@@ -1,3 +1,7 @@
+/*
+* Implements the only non-template function from the framework.
+*/
+
 #include "framework.h"
 
 using namespace std;
@@ -8,10 +12,12 @@ namespace amt {
 	vector<Hero> Hero::LoadHeroList() {
 		vector<Hero> retval;
 		ifstream ifs{ HeroInfoFile };
-		if (!ifs) throw FileException("Failed to open the hero info file: " + HeroInfoFile.string());
+		if (!ifs) 
+			throw FileException("Failed to open the hero info file: " + HeroInfoFile.string());
 		string line;
 		size_t n, m;
-		if(!getline(ifs, line)) throw FileException("Failed to read the hero info file header.");
+		if(!getline(ifs, line)) 
+			throw FileException("Failed to read the hero info file header.");
 		while (getline(ifs, line)) {
 			if (line.empty()) break;
 			Hero hero;
@@ -20,8 +26,10 @@ namespace amt {
 			hero.name = line.substr(0U, n);
 			hero.id = line.substr(n+1, m-(n+1));
 			hero.numSkins = stoi(line.substr(m + 1));
-			if (hero.id.length() != 2) throw FileException("Invalid class ID length in the hero info file, each class ID must consist of exactly 2 digits.");
-			if (hero.numSkins < 1 || hero.numSkins > 19) throw FileException("Invalid number of hero skins in the hero info file, each hero must have between 1 and 19 skins.");
+			if (hero.id.length() != 2) throw 
+				FileException("Invalid class ID length in the hero info file, each class ID must consist of exactly 2 digits.");
+			if (hero.numSkins < 1 || hero.numSkins > 19) 
+				throw FileException("Invalid number of hero skins in the hero info file, each hero must have between 1 and 19 skins.");
 			retval.push_back(move(hero));
 		}
 		return retval;
